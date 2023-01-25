5741e0a23213692bc148d844fdcd44e6a88f10fd
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2023-01-26 02:37:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ApiCall from './Components/ApiCall'
+
+const App = () => {
+    return (
+        <div>
+            <ApiCall/>
+        </div>
+    )
+}
+
+export default App
